0a6fce86201f49f01c45c2e3c98210bceafa88d8
蜂蜜柚子茶 <230248261@seu.edu.cn>
2025-09-08 17:30:43 +0800

    delate cuda kernel

--- cuda_dq_kernel/test/test_dq_inv.py
+++
@@ -1,9 +0,0 @@
-import torch
-import time
-import dq_torch
-from dqrobotics import DQ
-a = torch.tensor([1, 2, 3, 4, 0, 6, 7, 8], dtype=torch.float32, device='cuda:0').repeat(1,1)
-from dqrobotics import DQ
-dq_robotics_a = DQ(1, 2, 3, 4, 0, 6, 7, 8)
-print("dq_robotics_a: ", dq_robotics_a.inv())
-print("torch_a: ", dq_torch.inv(a))
